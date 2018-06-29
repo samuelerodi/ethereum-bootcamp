21f3adf8fd67784567eb356ad6b1a3a3240526fa
Samuele <samuele.rodi@it.pwc.com>
2018-06-29 16:32:34 +0200

    Fix secret

--- secret-template.js
+++ secret-template.js
@@ -4,6 +4,6 @@ module.exports = {
     kovan: 'myth like bonus scare over problem client lizard pioneer submit female collect', //Your mnemonic phrase
     rinkeby: 'myth like bonus scare over problem client lizard pioneer submit female collect', //Your mnemonic phrase
     mainnet: 'myth like bonus scare over problem client lizard pioneer submit female collect'  //Your mnemonic phrase
-  }
-  infura_apikey: 'XXXX'   //Your Infura Api Key
+  },
+  infuraApiKey: 'XXXX'   //Your Infura Api Key
 }
--- truffle.js
+++ truffle.js
@@ -10,26 +10,26 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     ropsten: {
-        provider: new HDWalletProvider(secret.mnemonic.ropsten, "https://ropsten.infura.io/" + secret.infura_apikey),
+        provider: new HDWalletProvider(secret.mnemonic.ropsten, "https://ropsten.infura.io/" + secret.infuraApiKey),
         network_id: 3,
         gasPrice: 3000000000,
         gas: 4500000
     },
     kovan: {
-        provider: new HDWalletProvider(secret.mnemonic.kovan, "https://kovan.infura.io/" + secret.infura_apikey),
+        provider: new HDWalletProvider(secret.mnemonic.kovan, "https://kovan.infura.io/" + secret.infuraApiKey),
         network_id: 42,
         gasPrice: 3000000000,
         gas: 4500000
 
     },
     rinkeby: {
-        provider: new HDWalletProvider(secret.mnemonic.rinkeby, "https://rinkeby.infura.io/" + secret.infura_apikey),
+        provider: new HDWalletProvider(secret.mnemonic.rinkeby, "https://rinkeby.infura.io/" + secret.infuraApiKey),
         network_id: 4,
         gasPrice: 3000000000,
         gas: 4500000
     },
     mainnet: {
-        provider: new HDWalletProvider(secret.mnemonic.mainnet, "https://mainnet.infura.io/" + secret.infura_apikey),
+        provider: new HDWalletProvider(secret.mnemonic.mainnet, "https://mainnet.infura.io/" + secret.infuraApiKey),
         network_id: 1,
         gasPrice: 2000000000,
         gas: 6000000
