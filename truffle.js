0b222d822fbcea49f41c62e00ef6184dc3202e7a
Samuele <samuele.rodi@it.pwc.com>
2018-07-17 11:28:54 +0200

    Fix drizzle 1.1.5

--- truffle.js
+++ truffle.js
@@ -7,7 +7,7 @@ module.exports = {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: 100 // Match any network id
     },
     ropsten: {
         provider: new HDWalletProvider(secret.mnemonic.ropsten, "https://ropsten.infura.io/" + secret.infuraApiKey),
